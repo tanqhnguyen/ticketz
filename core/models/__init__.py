69b134f33d27840b62d9aba1771be6db90a4ec97
Sina <si_al_ta@yahoo.com>
2013-11-08 13:41:25 +0200

    change __init__ file

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,6 @@
 from core.models.user import User
+from core.models.event import Event
+from core.models.sold_ticket import SoldTicket
+from core.models.ticket_type import TicketType
 
 __all__ = ['User','Event','SoldTicket','TicketType']
