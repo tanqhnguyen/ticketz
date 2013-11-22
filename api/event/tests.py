4b3e831391ff9392c05fb54f868141666612b0c0
Tan Nguyen <laoshanlung@gmail.com>
2013-11-22 18:16:45 +0200

    Add basic structure for testing APIs

---
+++ api/event/tests.py
@@ -0,0 +1,10 @@
+from core.tests import ApiTestCase
+from core.models import User, Event
+import json
+
+class EventTestCase(ApiTestCase):
+    def test_create_api(self):
+        pass
+
+    def test_update_api(self):
+        pass
---
+++ core/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from core.models import User
+import json
+from django.core.urlresolvers import reverse
+
+class ApiTestCase(TestCase):
+    fixtures = ['fixtures.json']
+
+    def setUp(self):
+        self.user = User.objects.order_by('-id')[0]
+        self.client.login(username=self.user.username, password='ticketz')
+
+    def post_json(self, url, data = {}):
+        response = self.client.post(reverse(url), json.dumps(data), content_type="application/json")
+        return json.loads(response.content)
