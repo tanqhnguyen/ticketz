11bc319bf1fa956bd342a9b956f3722246573779
basifat <babatunde.asifat@gmail.com>
2013-11-26 00:33:17 +0200

    #16 Setup Django Admin

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,15 @@
+
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import login, logout
-
 from core import views
+
+
 
 urlpatterns = patterns('',
     url(r'^register$', views.register, name='register'),
     url(r'^login$', login, {'template_name': 'core/login.html'}),
     url(r'^logout$', logout, {'next_page': '/'}),
     url(r'', views.IndexView.as_view()),
+    
+    
 )
--- ticketz/urls.py
+++ ticketz/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+admin.autodiscover()
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
-    url(r'', include('core.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'', include('social.apps.django_app.urls', namespace='social')),
+    #url(r'', include('core.urls')),
     # Examples:
     # url(r'^$', 'ticketz.views.home', name='home'),
     # url(r'^ticketz/', include('ticketz.foo.urls')),
@@ -15,5 +17,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    
 )
