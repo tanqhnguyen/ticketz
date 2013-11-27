a20824ab23f83b14e0a896a202cf4e47356e1440
Sina <si_al_ta@yahoo.com>
2013-11-27 17:35:03 +0200

    #2 create event and delete event

--- core/models/event.py
+++ core/models/event.py
@@ -1,15 +1,33 @@
+from django.core.serializers import json
 from django.db import models
-from core.models.user import User
+from core.models import User
 
 class Event(models.Model):
     page_attribute = models.TextField()
-    name = models.TextField(max_length=128)
-    description = models.TextField()
+    name = models.TextField(max_length=128 , default="New Event")
+    description = models.TextField(default="")
     type = models.TextField(max_length=16)
-    age_limit = models.IntegerField()
-    live_date = models.DateTimeField()
-    start_date = models.DateTimeField()
+    age_limit = models.IntegerField(default=None)
+    end_date = models.DateTimeField(default=None)
+    start_date = models.DateTimeField(default=None)
+    is_active = models.BooleanField(default=False)
     user = models.ForeignKey(User)
 
     class Meta:
         app_label = "core"
+
+    def setParams(self, request):
+            json_data = request.read()
+            # json_data contains the data uploaded in request
+            data = json.loads(json_data)
+            # data is now a Python dict representing the uploaded JSON.
+            id = self._get_pk_val
+            name = self.name
+            description = self.description
+            age_limit = self.age_limit
+            start_date = self.start_date
+            end_date = self.end_date
+            is_active = self.is_active
+            data = {"id":id,"name":name,"description":description,"age_limit":age_limit,
+                    "start_date":start_date,"end_date":end_date,"is_active":is_active}
+            return data
