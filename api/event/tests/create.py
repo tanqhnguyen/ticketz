4e13887ddc44366e368539052044b1828bade8d1
Tan Nguyen <laoshanlung@gmail.com>
2013-12-17 15:09:14 +0200

    Update tests

--- api/event/tests/create.py
+++ api/event/tests/create.py
@@ -5,6 +5,15 @@ import json
 class EventTestCase(ApiTestCase):
     def test_success_create_api(self):
         response = self.post_json("api_event_create")
-        self.assertEquals(response["error"], None)
+        print response
+        self.assertFalse("error" in response)
+        count = Event.objects.get(user_id=self.user.id).count()
+        self.assertEquals(count, 1)
+        self.assertIsNotNone(response["data"])
+
+    def test_create_event_twice(self):
+        self.post_json("api_event_create")
+        response = self.post_json("api_event_create")
+        self.assertFalse("error" in response)
         count = Event.objects.get(user_id=self.user.id).count()
         self.assertEquals(count, 1)
--- core/tests.py
+++ core/tests.py
@@ -8,7 +8,7 @@ class ApiTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.order_by('-id')[0]
-        self.client.login(username=self.user.username, password='ticketz')
+        self.client.login(username=self.user.username, password='123qwe')
 
     def post_json(self, url, data = {}):
         response = self.client.post(reverse(url), json.dumps(data), content_type="application/json")
