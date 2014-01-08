f7d9abb82e34dc2e97961400a396645423a3eaab
Tan Nguyen <laoshanlung@gmail.com>
2014-01-08 19:39:30 +0200

    Auto update wysiwyg control

--- static/js/models/controls/wysiwyg.js
+++ static/js/models/controls/wysiwyg.js
@@ -10,7 +10,17 @@ define([
     },
 
     onRender: function() {
+      var self = this;
       var attribute = Marionette.getOption(this, 'attribute');
+
+      this.options.onkeydown = function() {
+        self.model.set(attribute, self.$el.code());
+      }
+
+      this.options.onblur = function() {
+        self.model.set(attribute, self.$el.code());
+      }
+
       this.$el.summernote(this.options);
       this.$el.code(this.model.get(attribute));
     }
